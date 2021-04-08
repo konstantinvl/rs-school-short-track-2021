89eed7c235846566cc651bdbd76b354a05d02efa
konstantinvl <konstantinvl@inbox.ru>
2021-04-09 01:21:17 +0300

    feat: add 03

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,23 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  let array = arr;
+  array = array.filter((item) => item !== -1).sort((a, b) => a - b);
+  // return arr.sort((a, b) => {
+  //  if (a === -1) {
+  //    return 1;
+  //  }
+  //  return a - b;
+  // });
+  let count = 0;
+  return arr.map((item, index) => {
+    if (item === -1) {
+      count += 1;
+      return item;
+    }
+    return array[index - count];
+  });
 }
 
 module.exports = sortByHeight;
