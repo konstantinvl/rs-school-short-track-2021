8f1a5388e96dad18cae8fa5a2fe69020de862068
konstantinvl <konstantinvl@inbox.ru>
2021-04-11 16:32:54 +0300

    feat: add find index

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,13 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  return array.reduce((sum, item, index) => {
+    if (item === value) {
+      return sum + index;
+    }
+    return sum + 0;
+  }, 0);
 }
 
 module.exports = findIndex;
